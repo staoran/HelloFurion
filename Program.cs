a7ed7e90d873845fc534301a4911d27fd26e11a5
staoran <staoran@gmail.com>
2026-05-09 16:11:10 +0800

    Development 模式下，日志文件按日期切分（FileNameRule）失效 https://gitee.com/dotnetchina/Furion/issues/IJM4RN

--- Program.cs
+++ Program.cs
@@ -1,23 +1,41 @@
-using Furion.DataEncryption;
+using System;
+using System.IO;
+using Furion.UnifyResult;
+using Furion.Logging.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
-Serve.Run(services => { services.AddJwt(); });
+Serve.Run(x => x.AddFileLogging(options =>
+{
+    options.FileNameRule = fileName =>
+    {
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                              ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+                              ?? "Unknown";
+        var isDevelopment =
+            String.Equals(environmentName, "Development", StringComparison.OrdinalIgnoreCase);
+
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") +
+                          $" - {(isDevelopment ? "Development" : environmentName)} - " +
+                          fileName);
+        return string.Format(fileName, DateTime.Now);
+    };
+    options.WithStackFrame = true;
+    options.WithTraceId = true;
+    options.HandleWriteError = writeError => writeError.UseRollbackFileName(
+        $"{Path.GetFileNameWithoutExtension(writeError.CurrentFileName)}-oops{Path.GetExtension(writeError.CurrentFileName)}");
+}));
 
 [DynamicApiController]
 public class HelloService
 {
-    public string Say()
+
+    public void Say()
     {
-        var payload = new Dictionary<string, object>()
-        {
-            { "Id", "Id" },
-            { "Account", "Account" },
-            { "Name", "Name" },
-            { "OrgId", "Id" },
-            { "OrgName", "Name" },
-            { "IsAdmin", "isAdmin" }
-        };
-        // "IssuerSigningKey": "NCj3K8XNavMpXqXkNxyYB9XoKL4Xpef"
-        // 31 位
-        return JWTEncryption.Encrypt(payload);
+        "输出日志".LogInformation();
+    }
+
+
+    public void SayEmptySucceeded(int id)
+    {
     }
 }
